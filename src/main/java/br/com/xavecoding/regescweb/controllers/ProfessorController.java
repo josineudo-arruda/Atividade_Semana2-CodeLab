bef291eb3fba67726168e6492c5618d85186b1e9
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-18 15:04:34 -0300

    feat: create connection between professor table and mysql

--- src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
@@ -1,25 +1,22 @@
 package br.com.xavecoding.regescweb.controllers;
 
 import br.com.xavecoding.regescweb.models.Professor;
-import br.com.xavecoding.regescweb.models.StatusProfessor;
+import br.com.xavecoding.regescweb.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class ProfessorController {
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @GetMapping("/professores")
     public ModelAndView index() {
-        Professor batman = new Professor("batman", new BigDecimal(5000), StatusProfessor.AFASTADO);
-        batman.setId(1L);
-        Professor mulherMaravilha = new Professor("mulher maravilha", new BigDecimal(10000), StatusProfessor.ATIVO);
-        mulherMaravilha.setId(2L);
-
-        List<Professor> professores = Arrays.asList(batman, mulherMaravilha);
+        List<Professor> professores = this.professorRepository.findAll(); //devolve uma lista de professores
         ModelAndView mv = new ModelAndView("professores/index");
         mv.addObject("professores", professores);
         return mv;
