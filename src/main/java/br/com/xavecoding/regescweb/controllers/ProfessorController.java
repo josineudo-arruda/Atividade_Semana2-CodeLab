ca4d6236f954381e1c97a01ebef7afe2946881c7
Josineudo Arruda <96135176+josineudo-arruda@users.noreply.github.com>
2023-04-21 22:36:47 -0300

    feat: add show in professor

--- src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
+++ src/main/java/br/com/xavecoding/regescweb/controllers/ProfessorController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ProfessorController {
@@ -43,6 +45,19 @@ public class ProfessorController {
         } else {
             Professor professor = requisicao.toProfessor();
             this.professorRepository.save(professor); //inserção no bdd
+            return new ModelAndView("redirect:/professores/" + professor.getId());
+        }
+    }
+
+    @GetMapping("/professores/{id}")
+    public ModelAndView show(@PathVariable Long id) {
+        Optional<Professor> optional = this.professorRepository.findById(id); //para n ocorrer exception
+        if(optional.isPresent()) {
+            Professor professor = optional.get();
+            ModelAndView mv = new ModelAndView("professores/show");
+            mv.addObject("professor", professor);
+            return mv;
+        } else {
             return new ModelAndView("redirect:/professores");
         }
     }
